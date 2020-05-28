ef56eddc5ea40d76c55862ffab135929fd376054
huakaimay <huakaimay@gmail.com>
2020-05-28 16:43:08 +0800

    exercise 1.17

---
+++ Chapter 1/Exercise 1.17.rkt
@@ -0,0 +1,18 @@
+#lang racket/base
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+
+(define (double n)
+  (+ n n))
+
+(define (halve n)
+  (/ n 2))
+
+(define (multi a b)
+  (cond ((= b 0) 0)
+        ((even? b) (double (multi a (halve b))))
+        (else (+ a (multi a (- b 1))))))
+
+(multi 3 3)
