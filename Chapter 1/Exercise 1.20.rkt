63c020e1ccba4df3761b1acc3bedef61b9fe15ba
huakaimay <huakaimay@gmail.com>
2020-05-29 15:46:32 +0800

    exercise 1.20

---
+++ Chapter 1/Exercise 1.20.rkt
@@ -0,0 +1,29 @@
+#lang racket/base
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(gcd 206 40)
+(gcd 40 (remainder 206 40))
+(gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+(gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+     (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+(gcd (remainder 6 (remainder 40 6))
+     (remainder (remainder 40 6) (remainder 6 (remainder 40 6))))
+(gcd (remainder 6 4)
+     (remainder 4 (remainder 6 4)))
+(gcd 2
+     (remainder 4 2))
+(gcd 2 0)
+2
+
+
+(gcd 206 40)
+(gcd 40 6)
+(gcd 6 4)
+(gcd 4 2)
+(gcd 2 0)
+2
