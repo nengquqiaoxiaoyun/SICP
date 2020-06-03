0a8ec3aa308d72b318f6013cabd7c50e063a64af
huakaimay <huakaimay@gmail.com>
2020-06-03 11:24:17 +0800

    exercise 1.22

--- Chapter 1/Exercise 1.22.rkt
+++ Chapter 1/Exercise 1.22.rkt
@@ -1,25 +1,24 @@
 #lang racket/base
+
 (define (square x)
   (* x x))
 
 (define (runtime) 
      (current-milliseconds))
 
-; find smallest divisor
 (define (smallest-divisor n)
-      (find-divisor n 2))
+  (find-divisor n 2))
 
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
+        (else (find-divisor n (+ 1 test-divisor)))))
 
-; 除的尽说明最小公因数就是a
 (define (divides? a b)
-  (= (remainder b a) 0 ))
+  (= (remainder b a) 0))
 
 (define (prime? n)
-  ( = (smallest-divisor n) n))
+  (= (smallest-divisor n) n))
 
 
 (define (timed-prime-test n)
@@ -28,26 +27,29 @@
 (define (start-prime-test n start-time)
   (if (prime? n)
       (report-prime n (- (runtime) start-time))
-      (display "")))
+      #f))
 
 (define (report-prime n elapsed-time)
-   
   (display n)
   (display " *** ")
   (display elapsed-time)
- (newline)
- )
+  (newline))
 
-(define (seach-for-prime lower upper)
-  (define (iter n)
-    (cond ((<= n upper)
-           (timed-prime-test n)
-           (iter (+ n 2)))))
-  (iter
-   (if (odd? lower)
-      lower
-      (+ lower 1))))
+(define (search-for-primes n count)
+  (if (even? n)
+      (s-f-p (+ n 1) count)
+      (s-f-p n count)))
 
-(seach-for-prime 10000 10099)
+(define (s-f-p n count)
+  (if (> count 0)
+      (if (timed-prime-test n)
+          (s-f-p (+ n 2) (- count 1))
+          (s-f-p (+ n 2) count))
+      "computation complate"))
+  
 
 
+(search-for-primes 1000 3)
+(search-for-primes 10000 3)
+(search-for-primes 100000 3)
+(search-for-primes 1000000 3)
